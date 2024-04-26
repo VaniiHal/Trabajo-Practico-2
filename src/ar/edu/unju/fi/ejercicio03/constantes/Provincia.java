2ce6bcdd6617736140d7e52de84fe5a9adb7399b
VaniiHal <VaniiHal@gmail>
2024-04-26 12:11:58 -0300

    Ejercicio 3 resuelto y funcional

--- src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
+++ src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
@@ -1,5 +1,38 @@
 package ar.edu.unju.fi.ejercicio03.constantes;
 
 public enum Provincia {
+	JUJUY(1_121_183, 53_219), 
+	SALTA(1_579_051, 155_488),
+	TUCUMAN(1_677_130, 22_524),
+	CATAMARCA(418_991, 102_602),
+	LA_RIOJA(393_531, 89_680),
+	SANTIAGO_DEL_ESTERO(950_041, 136_351);
 
+	private int poblacion;
+	private int superficie;
+	
+	private Provincia(int poblacion, int superficie) {
+		this.poblacion = poblacion;
+		this.superficie = superficie;
+	}
+
+	public int getPoblacion() {
+		return poblacion;
+	}
+
+	public void setPoblacion(int poblacion) {
+		this.poblacion = poblacion;
+	}
+
+	public int getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(int superficie) {
+		this.superficie = superficie;
+	}
+	
+	public double calcularDensidad() {
+		return poblacion/superficie;
+	}
 }
--- src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -1,10 +1,18 @@
 package ar.edu.unju.fi.ejercicio03.main;
 
+import ar.edu.unju.fi.ejercicio03.constantes.Provincia;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Provincia[] provincias = Provincia.values();
+		for (Provincia prov : provincias) {
+			int pob = prov.getPoblacion();
+			int sup = prov.getSuperficie();
+			double den = prov.calcularDensidad();
+			System.out.println("Provincia: " + prov + "\tPoblacion: " + pob + "\tSuperficie: " + sup + "\tDensidad: " + den);
+		}
+		
 	}
 
 }
